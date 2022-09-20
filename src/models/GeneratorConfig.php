ec7200cf273f119007ccbde4b736a6a02aa6fb7a
Joshua Pease <joshua.pease@viget.com>
2022-09-20 12:28:45 -0700

    [n/a] Remove .twig extension

--- src/models/GeneratorConfig.php
+++ src/models/GeneratorConfig.php
@@ -6,12 +6,12 @@ class GeneratorConfig
 {
     // Eventually this should be based on a module config file
     public function __construct(
-        public string $defaultLayout = '_layouts/app.twig',
+        public string $defaultLayout = '_layouts/app',
         public string $componentPath = '_components',
         public string $partsKitPath = 'parts-kit',
-        public string $partsKitLayout = 'viget-base/_layouts/parts-kit.twig',
+        public string $partsKitLayout = 'viget-base/_layouts/parts-kit',
     )
     {
-    
+
     }
 }
